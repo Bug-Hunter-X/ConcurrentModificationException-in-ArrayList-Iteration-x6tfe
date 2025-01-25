f3d49ce19882b25453d43c7d05ca54e8681a6830
BugHunter-X <askzenai@gmail.com>
2025-01-25 17:44:05 +0100

    Add ConcurrentModificationExceptionSolution.java file

---
+++ ConcurrentModificationExceptionSolution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ConcurrentModificationExceptionSolution {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("apple", "banana", "cherry"));
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String item = iterator.next();
+            if (item.equals("banana")) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list); // Output: [apple, cherry]
+    }
+}
